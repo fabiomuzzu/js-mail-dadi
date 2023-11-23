99c6a632fd8c4154ba74e1bbe3501646dbfcae97
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-23 20:59:05 +0100

    added if/else in second excercise

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -1,9 +1,18 @@
 // Generazione numero random per player
-const rnd_player = Math.floor(Math.random() * 6) + 1
+const rnd_player = Math.floor(Math.random() * 6) + 1;
 console.log(rnd_player);
 
 // Generazione numero random per computer
-const rnd_computer = Math.floor(Math.random() * 6) + 1
+const rnd_computer = Math.floor(Math.random() * 6) + 1;
 console.log(rnd_computer);
 
-
+// Creazione condizione if/else per stabilire il vincitore
+if(rnd_player == rnd_computer){
+    console.log("Pareggio!")
+}
+else if(rnd_player > rnd_computer){
+    console.log("Hai vinto!")
+}
+else{
+    console.log("Hai perso!")
+}
