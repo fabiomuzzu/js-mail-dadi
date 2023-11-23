f5a523ef414804d2ce7b136114a4c09a2ab45acb
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-23 17:37:42 +0100

    created if/else condition in for

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,9 +1,16 @@
 let email = prompt('Inserisci la tua mail');
-console.log(email);
+//console.log(email);
 
 let valid_email = ["mario@gmail.com", "marco@gmail.com", "agostino@gmail.com"];
-console.log(valid_email);
 
-for(){
+// console.log(valid_email.length);
 
+for(let i = 0; i < valid_email.length; i++){
+    // console.log(valid_email[i]);
+    if (email == valid_email[i]) {
+        console.log('Accesso consentito!')
+    } 
+    else {
+        console.log('Accesso negato!')
+    }
 }
