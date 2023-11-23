f78631c8c2d31684dd3e7d792c7ffb2fcc566620
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-23 18:36:11 +0100

    end of excercise 1

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,16 +1,12 @@
 let email = prompt('Inserisci la tua mail');
-//console.log(email);
 
 let valid_email = ["mario@gmail.com", "marco@gmail.com", "agostino@gmail.com"];
 
-// console.log(valid_email.length);
 
+let verified_email = false;
 for(let i = 0; i < valid_email.length; i++){
-    // console.log(valid_email[i]);
     if (email == valid_email[i]) {
-        console.log('Accesso consentito!')
-    } 
-    else {
-        console.log('Accesso negato!')
+        verified_email = true;
     }
 }
+console.log(verified_email);
