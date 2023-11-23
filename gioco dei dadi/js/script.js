54e997b54f9b8ee178bc6ef7f0a280ee031bdf58
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-23 19:32:03 +0100

    added random number generator in second excercise

---
+++ gioco dei dadi/js/script.js
@@ -0,0 +1,9 @@
+// Generazione numero random per player
+const rnd_player = Math.floor(Math.random() * 6) + 1
+console.log(rnd_player);
+
+// Generazione numero random per computer
+const rnd_computer = Math.floor(Math.random() * 6) + 1
+console.log(rnd_computer);
+
+
