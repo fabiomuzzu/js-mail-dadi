c31f368756429f0143767b177fb5c32ec678d1b2
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-23 16:43:39 +0100

    starting for cicle

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,5 +1,9 @@
 let email = prompt('Inserisci la tua mail');
 console.log(email);
 
-let vaild_email = ["mario@gmail.com", "marco@gmail.com", "agostino@gmail.com"];
+let valid_email = ["mario@gmail.com", "marco@gmail.com", "agostino@gmail.com"];
 console.log(valid_email);
+
+for(){
+
+}
